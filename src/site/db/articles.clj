f0dc817a1a7fd00132816cf563821d12185e1d1d
Theodore Randolph <trandolp@gmail.com>
2014-08-29 15:42:26 -0700

    Slight formatting change.

--- src/site/db/articles.clj
+++ src/site/db/articles.clj
@@ -8,7 +8,8 @@
                                                      "WHERE published_at < CURRENT_TIMESTAMP")]) first :count)
           first-article (* page-number 10)
           articles-on-page (sql/query db [(str "SELECT created_at, published_at, title, body FROM articles "
-                                               "WHERE published_at < CURRENT_TIMESTAMP ORDER BY published_at "
-                                               "DESC LIMIT 10 OFFSET ?") first-article])]
+                                               "WHERE published_at < CURRENT_TIMESTAMP ORDER BY "
+                                               "published_at DESC LIMIT 10 OFFSET ?") first-article])]
       (with-meta articles-on-page {:prev-page (> page-number 0) 
                                    :next-page (> number-of-articles (+ first-article 10))}))))
+
