23e350936aca8e76d561e3051d23bce5d73a6f5a
Theodore Randolph <trandolp@gmail.com>
2014-08-29 15:37:25 -0700

    First crude attempt to get data out of the thing.

--- src/site/db/connect.clj
+++ src/site/db/connect.clj
@@ -1,13 +1,40 @@
 (ns site.db.connect
-  (:require [environ.core]
-            ;;[clojure.java.jdbc]
-            )
-  (:import com.mchange.v2.c3p0.ComboPooledDataSource))
+  (:require environ.core
+            [clojure.java.jdbc :as sql])
+  (:import com.mchange.v2.c3p0.ComboPooledDataSource
+           java.net.URI))
 
 (def spec (:database-url environ.core/env))
 
-(defn connect [] (let [cpds (doto (ComboPooledDataSource.)
-                              (.setJdbcUrl spec))]
-                   cpds))
+(defn- connect[] (let [uri (URI. spec)
+                       userinfo (.split (.getUserInfo uri) ":")
+                       username (nth userinfo 0)
+                       password (nth userinfo 1)
+                       dburl (str "jdbc:postgresql://" (.getHost uri) ":" (.getPort uri) (.getPath uri))
+                       cpds (doto (ComboPooledDataSource.)
+                              (.setJdbcUrl dburl)
+                              (.setUser username)
+                              (.setPassword password))
+                       db {:datasource cpds}
+                       hastable (-> (sql/query db (str "select count(table_name) from "
+                                                       "information_schema.tables "
+                                                       "where table_name = 'articles'"))
+                                    first :count pos?)]
+                   (when (not hastable)
+                     (sql/with-db-transaction [t db]
+                       (sql/db-do-commands
+                        t
+                        (sql/create-table-ddl :articles
+                                              [:id :serial "PRIMARY KEY"]
+                                              [:created_at :timestamp 
+                                               "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+                                              [:published_at :timestamp
+                                               "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+                                              [:title :varchar "NOT NULL"]
+                                              [:body :varchar "NOT NULL"])
+                        (sql/create-table-ddl :version
+                                              [:version :serial "PRIMARY KEY"]))
+                       (sql/insert! t :version nil [1])))
+                   db))
 
 (def pool (connect))
