d9213c35db5bd6d22d32f4c575d2c7adf4da7dd7
Theodore Randolph <trandolp@gmail.com>
2014-08-29 15:36:18 -0700

    First attempt at getting articles out of the thing.

--- src/site/db/articles.clj
+++ src/site/db/articles.clj
@@ -1,3 +1,14 @@
 (ns site.db.articles
-  (:require site.db.connect
+  (:require [site.db.connect :refer [pool]]
             [clojure.java.jdbc :as sql]))
+
+(defn articles [page-number]
+  (sql/with-db-transaction [db pool :isolation :serializable]
+    (let [number-of-articles (-> (sql/query db [(str "SELECT count(id) FROM articles "
+                                                     "WHERE published_at < CURRENT_TIMESTAMP")]) first :count)
+          first-article (* page-number 10)
+          articles-on-page (sql/query db [(str "SELECT created_at, published_at, title, body FROM articles "
+                                               "WHERE published_at < CURRENT_TIMESTAMP ORDER BY published_at "
+                                               "DESC LIMIT 10 OFFSET ?") first-article])]
+      (with-meta articles-on-page {:prev-page (> page-number 0) 
+                                   :next-page (> number-of-articles (+ first-article 10))}))))
