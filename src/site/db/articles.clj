fb32cad8ffe762282ccc9e0fcc983e4af418ae9b
Theodore Randolph <trandolp@gmail.com>
2014-08-29 19:13:11 -0700

    Start trying a simple template. And fix a thing with the articles loader.

--- src/site/db/articles.clj
+++ src/site/db/articles.clj
@@ -4,12 +4,17 @@
 
 (defn articles [page-number]
   (sql/with-db-transaction [db pool :isolation :serializable]
-    (let [number-of-articles (-> (sql/query db [(str "SELECT count(id) FROM articles "
+    (let [articles-per-page 10
+          number-of-articles (-> (sql/query db [(str "SELECT count(id) FROM articles "
                                                      "WHERE published_at < CURRENT_TIMESTAMP")]) first :count)
-          first-article (* page-number 10)
+          first-article (* page-number articles-per-page)
           articles-on-page (sql/query db [(str "SELECT created_at, published_at, title, body FROM articles "
                                                "WHERE published_at < CURRENT_TIMESTAMP ORDER BY "
-                                               "published_at DESC LIMIT 10 OFFSET ?") first-article])]
+                                               "published_at DESC LIMIT ? OFFSET ?")
+                                          articles-per-page first-article])]
       (with-meta articles-on-page {:prev-page (> page-number 0) 
-                                   :next-page (> number-of-articles (+ first-article 10))}))))
+                                   :next-page (> number-of-articles (+ first-article articles-per-page))
+                                   :total-pages
+                                   (+ (quot number-of-articles articles-per-page)
+                                      (if (pos? (rem number-of-articles articles-per-page)) 1 0))}))))
 
